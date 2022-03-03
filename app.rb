e39af95cd9333c390f45eee6174a1f0c6b20c2b4
张晋铭 <2249023152@qq.com>
2022-03-03 18:09:18 +0800

    remove py part

--- app.rb
+++ app.rb
@@ -1,68 +1,71 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'sinatra'
+require 'sinatra/base'
 
-# use this to display the fontawesome icons in the attrs
-set :haml, escape_attrs: false
+class MyApp < Sinatra::Base
+  # use this to display the fontawesome icons in the attrs
+  set :haml, escape_attrs: false
 
-basic_info = '404 Not Found: The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.'
+  basic_info = '404 Not Found: The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.'
 
-get '/home/?' do
-  haml :index
-end
-
-get '/' do
-  status 302
-  headers 'Location' => '/home'
-end
-
-get '/index/static_files/*' do |path|
-  send_file "./static_files/#{path}"
-end
-
-get '/index/*' do |path|
-  status 301
-  headers 'Location' => "/#{path}"
-end
-
-get '/static_files/*' do |path|
-  send_file "./static_files/#{path}"
-end
-
-get '/favicon.ico' do
-  send_file './static_files/icons/duck.ico'
-end
-
-get '/s/?' do
-  haml :result
-end
-
-get '/notes/?' do
-  haml :notes
-end
-
-get %r{/notes/(\d+)/?} do |num|
-  begin
-    haml :notes, locals: { num: num }
-  rescue StandardError => e
-    haml :error, locals: { error_info: e }
+  get '/home/?' do
+    haml :index
   end
-end
 
-get '/links/?' do
-  haml :links
-end
+  get '/' do
+    status 302
+    headers 'Location' => '/home'
+  end
 
-get '/rss/?' do
-  headers 'Content-Type' => 'application/xml'
-  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" << haml(:rss)
-end
+  get '/index/static_files/*' do |path|
+    send_file "./static_files/#{path}"
+  end
 
-not_found do
-  haml :error, locals: { error_info: basic_info }
-end
+  get '/index/*' do |path|
+    status 301
+    headers 'Location' => "/#{path}"
+  end
 
-error do
-  'Sorry there was a nasty error - ' << env['sinatra.error'].message
+  get '/static_files/*' do |path|
+    send_file "./static_files/#{path}"
+  end
+
+  get '/favicon.ico' do
+    send_file './static_files/icons/duck.ico'
+  end
+
+  get '/s/?' do
+    haml :result
+  end
+
+  get '/notes/?' do
+    haml :list_notes
+  end
+
+  get %r{/notes/(\d+)/?} do |num|
+    begin
+      haml :notes, locals: { num: num }
+    rescue StandardError => e
+      status 500
+      haml :error, locals: { error_info: e, status_num: '500' }
+    end
+  end
+
+  get '/links/?' do
+    haml :links
+  end
+
+  get '/rss/?' do
+    headers 'Content-Type' => 'application/xml'
+    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" << haml(:rss)
+  end
+
+  not_found do
+    haml :error, locals: { error_info: basic_info, status_num: '404' }
+  end
+
+  error do
+    'Sorry there was a nasty error - ' << env['sinatra.error'].message
+  end
 end
--- runserver.py
+++
@@ -1,5 +0,0 @@
-from waitress import serve
-
-import demo0815
-
-serve(demo0815.app, host='0.0.0.0', port=8080)
